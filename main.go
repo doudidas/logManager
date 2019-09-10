c62a13c250a57b14752423c1493f2a245bb011d2
Edouard Topin <ed.topin@gmail.com>
2019-09-10 18:06:59 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package smartsearchlog
+
+import "fmt"
+
+func customLog(s string) {
+	fmt.Println("[LOG]" + s)
+}
+
+func customWarn(s string) {
+	fmt.Println("[WARNING]" + s)
+}
+func customErr(s string) {
+	fmt.Println("[ERROR]" + s)
+}
